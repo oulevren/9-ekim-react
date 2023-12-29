aea1dfbf36b7ab9b1f6420bdda62118a076c236b
evologhy <107946503+oulevren@users.noreply.github.com>
2023-12-29 14:12:07 +0300

    Add files via upload

---
+++ src/Components/Anasayfa.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Anasayfa(props) {
+
+
+
+
+  return (
+
+
+
+    <>
+    
+    <li>{props.isim}</li>
+    <li>menü</li>
+    <li>menü</li>
+
+    </>
+
+
+
+  )
+}
